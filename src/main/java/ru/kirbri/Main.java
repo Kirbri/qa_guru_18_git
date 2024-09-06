7d688f96d6024a564ef0e30a9cbf7a6d3d7c0c7f
Kirbri <61042916+Kirbri@users.noreply.github.com>
2024-09-06 11:24:30 +0300

    Update Main.java
    Add "!!!!"

--- src/main/java/ru/kirbri/Main.java
+++ src/main/java/ru/kirbri/Main.java
@@ -2,7 +2,7 @@ package ru.kirbri;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa.guru!");
-        System.out.println("version 2.0");
+        System.out.println("Hello qa.guru!!!!!!");
+        System.out.println("version 2.0!!!!");
     }
 }
