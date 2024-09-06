970a37ab68837d92c79022ed601b0884835ff5e4
Kirbri <61042916+Kirbri@users.noreply.github.com>
2024-09-06 11:27:01 +0300

    Update Main.java
    Add variblr msg

--- src/main/java/ru/kirbri/Main.java
+++ src/main/java/ru/kirbri/Main.java
@@ -1,8 +1,11 @@
 package ru.kirbri;
 
 public class Main {
+
+    private static String msg = "Hello qa.guru!!!!!!";
+    
     public static void main(String[] args) {
-        System.out.println("Hello qa.guru!!!!!!");
+        System.out.println(msg);
         System.out.println("version 2.0!!!!");
     }
 }
