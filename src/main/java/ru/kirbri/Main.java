bbf97b907915bcc182ddb025d0758529f1dd24ac
Ann Kornilova <Kirbri@yandex.ru>
2024-09-06 11:27:41 +0300

    added getMessage() method

--- src/main/java/ru/kirbri/Main.java
+++ src/main/java/ru/kirbri/Main.java
@@ -2,7 +2,11 @@ package ru.kirbri;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa.guru!");
+        System.out.println(getMessage());
         System.out.println("version 2.0");
+    }
+
+    private static String getMessage() {
+        return "Hello qa.guru!";
     }
 }
